c173a8d1b9f886d2a6d101a53ddd9568341fe9e2
huadee1108 <981929654@qq.com>
2019-01-27 23:29:44 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,42 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container" id="app">
+        <h1>在线翻译</h1>
+        <h5 class="text-muted">简单 / 易用 /便捷</h5>
+        <translate-form v-on:form-translate="translateText"></translate-form>
+        <translate-output v-text="translatedText"></translate-output>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script>
+    import TranslateForm from './components/TranslateForm'
+    import TranslateOutput from './components/TranslateOutput'
+    export default {
+        name: "app",
+        data:function(){
+          return{
+              translatedText: ""
+          }
+        },
+        components:{
+            TranslateForm,TranslateOutput
+        },
+        methods:{
+            translateText:function (text,language) {
+               this.axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate?' +
+                   'key=trnsl.1.1.20190127T140527Z.f11e7621a129706c.e20911bb07e4ea198c723e85f83928e3878565d1&' +
+                   'lang='+language+'&text='+text)
+                   .then((response) =>{
+                       // console.log(response.data.text[0]);
+                       this.translatedText = response.data.text[0];
+                   })
+            }
+        }
+    }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #app{
+        margin: 50px auto;
+        color: #2c3e50;
+    }
 </style>
---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="row" id="translateForm">
+        <div class="col-md-6 col-md-offset-3">
+            <form class="well form-inline" v-on:submit="formSubmit">
+                <input class="form-control" type="text" v-model="textToTranslate" placeholder="请输入要翻译的内容"/>
+                <select class="form-control" v-model="language">
+                    <option value="en">English</option>
+                    <option value="ru">Russian</option>
+                    <option value="ko">korean</option>
+                    <option value="ja">Janpenese</option>
+                </select>
+                <input class="btn btn-info" type="submit" value="翻译"/>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "translateForm",
+        data:function(){
+          return{
+              textToTranslate: "",
+              language: ""
+          }
+        },
+        methods:{
+            formSubmit:function (e) {
+                // alert(this.textToTranslate);
+                this.$emit("form-translate",this.textToTranslate,this.language);
+                e.preventDefault();
+            }
+        },
+        created:function () {
+            this.language = "en";
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
